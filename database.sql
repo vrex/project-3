e8856b3948406634420674877dd061da6d5862dd
vrex <magarvarad2@.com>
2026-07-15 01:21:27 +0530

    Initial commit

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE internship;
+
+USE internship;
+
+CREATE TABLE students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100),
+    email VARCHAR(100)
+);
---
+++ queries.sql
@@ -0,0 +1,10 @@
+INSERT INTO students(name,email)
+VALUES
+('Varad','varad@example.com'),
+('Rahul','rahul@example.com');
+
+SELECT * FROM students;
+
+SHOW DATABASES;
+
+SHOW TABLES;
